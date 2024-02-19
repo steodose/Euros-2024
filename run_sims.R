a5e7eb4208eeb459636abd66a2285cb1279bf355
Stephan Teodosescu <32054537+steodose@users.noreply.github.com>
2024-02-19 15:01:45 -0800

    Building website

--- run_sims.R
+++ run_sims.R
@@ -16,3 +16,14 @@ run_date <- case_when(lubridate::hour(Sys.time()) <= 9 ~as.Date(Sys.Date()),
 run_date <- Sys.Date()
 
 ### Coefficients
+
+
+### Read in Ratings and Schedule
+df_ratings <- read_csv('rankings.csv')
+schedule <- 
+  read_csv('schedule.csv') %>% 
+  mutate('team1_score' = ifelse(date >= run_date, NA, team1_score),
+         'team2_score' = ifelse(date >= run_date, NA, team2_score)) %>% 
+  mutate('team1_score' = case_when(is.na(shootout_winner) ~ team1_score,
+                                   shootout_winner == team1 ~ 0.1 + team1_score,
+                                   shootout_winner == team2 ~ -0.1 + team1_score))
